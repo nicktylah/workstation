5a76c7775fb2a681eca0c31ede3f4c7030013c75
Nick Tyler <tyler34862@gmail.com>
2022-05-06 11:21:18 -0400

    adds vimplug setup

--- setup.sh
+++ setup.sh
@@ -5,4 +5,7 @@ ln -s ~/workstation/vim/.vimrc ~/.vimrc && \
 echo "Symlinking .zshrc..." && \
 ln -s ~/workstation/zsh/.zshrc ~/.zshrc && \
 echo "Symlinking .aliases..." && \
-ln -s ~/workstation/aliases/.aliases ~/.aliases;
+ln -s ~/workstation/aliases/.aliases ~/.aliases && \
+echo "Installing vimplug..." && \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
